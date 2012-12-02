68a02aad29d0e449b43e8829c00c7cd383d92305
Jimmy Eaton <jimmy@jimmyeaton.com>
2012-12-02 11:16:36 -0600

    facebook ominiauth

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -54,9 +54,10 @@ module ApplicationHelper
       user.avatar
    else
   #    default_url = "#{root_url}images/guest.png"
-     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-   "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
-    url = Gravatar.new(user.email).image_url
+   #  gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+   #"http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
+   # url = Gravatar.new(user.email).image_url
+   nil
   end
 end
 
