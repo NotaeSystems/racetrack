44ae7fa0af3ad55f63837d6de55ae2ac3476234c
Jimmy Eaton <jimmy@jimmyeaton.com>
2012-12-15 18:09:26 -0600

    twitter and facebook icons

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @tracks = Track.open.page(params[:page]).per_page(30).order('name')
+    @tracks = Track.active.page(params[:page]).per_page(30).order('name')
     @cards = Card.where("status = 'Open' ").page(params[:page]).per_page(30).order('name')
 
   end
