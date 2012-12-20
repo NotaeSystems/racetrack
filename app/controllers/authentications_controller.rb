48ed5ed93ff5a153dd4d7563b3af7ba9ecdf572d
Jimmy Eaton <jimmy@jimmyeaton.com>
2012-12-20 14:31:24 -0600

    registering existing user

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -70,7 +70,7 @@ def create
       new_user = @authentication.create_new_user(auth)
       new_user.add_achievement('Neophyte', session[:provider])
       session[:provider] = auth['provider']
-      logger.info = "New user is #{new_user.name unless new_user.blank?}"
+      logger.info "New user is #{new_user.name unless new_user.blank?}"
       session[:user_id] = new_user.id
       @authentication.user = current_user
       @authentication.save()
