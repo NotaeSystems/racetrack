aabd0960f186369213cd8d68338fb86795cb682e
Jimmy Eaton <jimmy@jimmyeaton.com>
2012-12-21 13:03:09 -0600

    permissions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   around_filter :user_time_zone, if: :current_user
-
+ 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
@@ -9,9 +9,13 @@ class ApplicationController < ActionController::Base
 
 
   def user_is_track_manager?(track)
+   return false if track.nil?
    return false if current_user.nil?
    return true if current_user.has_role?('admin')
-   current_user.is_track_manager?(track)
+   return true if track.owner_id == self.id
+   trackuser = Trackuser.where(:user_id => current_user.id, :track_id => track.id, :status => 'Manager').first
+   return true unless trackuser.blank?
+   false
   end
 
 
