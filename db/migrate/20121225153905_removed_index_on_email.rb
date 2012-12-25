4230d2ea5d234b4f4d90aa45c7990f20b5a0bdac
Jimmy Eaton <jimmy@jimmyeaton.com>
2012-12-25 09:49:19 -0600

    gates and facebook saas

---
+++ db/migrate/20121225153905_removed_index_on_email.rb
@@ -0,0 +1,9 @@
+class RemovedIndexOnEmail < ActiveRecord::Migration
+  def up
+     remove_index(:users, :name => 'index_users_on_email')
+     add_index :users, :email
+  end
+
+  def down
+  end
+end
