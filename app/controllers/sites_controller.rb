ed4a6a4ac6ba1490e5723a84766453c70bcc2c2e
Jimmy Eaton <jimmy@jimmyeaton.com>
2012-12-28 09:39:00 -0600

    leaderboards

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -6,7 +6,7 @@ class SitesController < ApplicationController
   def leaderboard
     @users = @site.users.page(params[:page]).per_page(30).order('amount DESC')
     if current_user
-      @myrank = @site.users.order('amount DESC').index(current_user)
+      @myrank = @site.users.order('amount DESC').index(current_user) + 1
     else
       @myrank = 'Not Ranked'
     end
